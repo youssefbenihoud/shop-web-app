e59fae202c4efdb7e52513800a5fd6b7b3abceb2
Youssef <user@Air-von-User.fritz.box>
2024-10-12 17:33:49 +0200

    update project to deploy

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const orderRoutes = require('./routes/orderRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://admin:aWO05VevmH5CREOl@shop-web-app.wkpdh.mongodb.net/';
+
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://admin:aWO05VevmH5CREOl@shop-web-app.wkpdh.mongodb.net/', {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
